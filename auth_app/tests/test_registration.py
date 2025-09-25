6a3b8692d27fe96ad0f643c81c5057c34df1e8fa
Fabian-85 <fabian.maier0805@hotmail.com>
2025-09-25 21:41:09 +0200

    test: write test for password validation in registration

--- auth_app/tests/test_registration.py
+++ auth_app/tests/test_registration.py
@@ -10,31 +10,46 @@ class RegistrationTests(APITestCase):
         self.user = User.objects.create_user(
             username='Fabian', email='fabian@mail.de', password='11111111')
 
-    def test_create_new_user(self):
+    def test_create_user_returns_200(self):
         url = reverse('register')
         response = self.client.post(url, {
             'username': 'testuser',
             'email': 'test_user@mail.de',
-            'password': 'testuser123'
+            'password': 'testuser123',
+            'confirmed_password': 'testuser123'
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_create_existing_username(self):
+    def test_create_user_with_existing_username_returns_400(self):
         url = reverse('register')
 
         response = self.client.post(url, {
             'username': 'Fabian',
             'email': 'test_user@mail.de',
-            'password': 'testuser123'
+            'password': 'testuser123',
+            'confirmed_password': 'testuser123'
         })
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_create_existing_email(self):
+    def test_create_user_with_existing_email_returns_400(self):
         url = reverse('register')
 
         response = self.client.post(url, {
             'username': 'testuser',
             'email': 'fabian@mail.de',
-            'password': 'testuser123'
+            'password': 'testuser123',
+            'confirmed_password': 'testuser123'
         })
         self.assertEqual(response.status_code,  status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_with_wrong_confirmed_password_returns_400(self):
+        url = reverse('register')
+
+        response = self.client.post(url, {
+            'username': 'testuser',
+            'email': 'fabian@mail.de',
+            'password': 'testuser123',
+            'confirmed_password': 'testuser'
+        })
+        self.assertEqual(response.status_code,  status.HTTP_400_BAD_REQUEST)
+
